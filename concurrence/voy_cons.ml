d8ba73d0ce1406b0caab62bf63bc85631e8606d8
Fati <reihaneh77@yahoo.com>
2018-06-19 10:47:11 +0200

    compiling problem solved

--- concurrence/voy_cons.ml
+++ concurrence/voy_cons.ml
@@ -10,6 +10,7 @@ let b = Js.Opt.get opt (fun() -> failwith "erreur")
 
 (*On récupère le dom HTML de la balise p (qui a pour id = "count")*)
 let d = Dom_html.getElementById "count"
+let d2 = Dom_html.getElementById "count2"
 
 
 (*On initalise l'objet input (là où l'utilisateur saisie la valeure) en récupérant avec le DOM HTML l'objet qui a pour id 'textF' puis on le 'coerce' en valuer Js_of_caml*)
@@ -18,9 +19,7 @@ let input = Dom_html.getElementById "textF"
 let opt2 = Dom_html.CoerceTo.input input
 let i = Js.Opt.get opt2 (fun() -> failwith "erreur")
 
-
-(*Dans le champs handler du bouton on met la fonction que l'on vient de créer qui nous permet donc de définir la fonction clicked comme une action qui se declenche*)
-
+(*fonction qui renvoie le nombre de voyelles dans une chaine de caracteres*)
 let rec nbr_voys str i n =
 	match (str,i, n) with
 	| ("",i, n) -> -1
@@ -28,27 +27,36 @@ let rec nbr_voys str i n =
 	| (str, i, n) when str.[i] = 'a' || str.[i] = 'e' || str.[i] = 'i' || str.[i] = 'o' || str.[i] = 'y' || str.[i] = 'u' || str.[i] = 'A' || str.[i] = 'E' || str.[i] = 'I' || str.[i] = 'O' || str.[i] = 'Y' || str.[i] = 'U' -> 1 + nbr_voys str (i+1) n
 	| (str, i, n) -> nbr_voys str (i+1) n;;
 
+(*fonction qui renvoie le nombre de quelques caracteres speciaux*)
+let rec nbr_space str i n =
+  match (str,i, n) with
+  | ("",i, n) -> -1
+	| (str,i, n) when i = n -> 0
+	| (str, i, n) when str.[i] = ' ' || str.[i] = '*' || str.[i] = '-' || str.[i] = '+'|| str.[i] = '/'|| str.[i] = '_'-> 1 + nbr_space str (i+1) n
+	| (str, i, n) -> nbr_space str (i+1) n;;
+
+(*fonction qui renvoie le nombre de consonnes*)
 let nbr_cons str =
- ( String.length str ) - nbr_voys str 0 (String.length str);;
+ ( String.length str ) - nbr_voys str 0 (String.length str) - nbr_space str 0 (String.length str) ;;
 
-let a = Array.make 2 0;;
-
+(*Thread qui change la valeur de la proprieté innerHTML de l'element deg avec le nombre de consonnes de la string passée en parametre*)
 let nbr_cons2 = fun n deg->
-(*  a.(0)<-nbr_cons n; *)
  (Lwt.return(deg##.innerHTML := (Js.string (string_of_int(nbr_cons n)))))
 
+
+(*Thread qui change la valeur de la proprieté innerHTML de l'element deg avec le nombre de voyelles de la string passée en parametre*)
 let nbr_voys2 = fun n deg->
-(*  a.(1)<-nbr_voys n 0 (String.length n); *)
  (Lwt.return(deg##.innerHTML := (Js.string (string_of_int(nbr_voys n 0 (String.length n))))))
 
-(*La fontion qui prend en paramètre un objet bouton, un objet input et un objet <p> : là ou le résultat s'affiche*)
-let clicked i deg=
-  Lwt_main.run (Lwt.bind (Lwt.return(Js.to_string(i##.value))) (fun s -> (Lwt.join [(nbr_voys2 s deg) ;(nbr_cons2 s deg)])))
+(*La fontion qui prend en paramètre un objet bouton, un objet input et un objet <p> puis lance les deux thread qui affiche le nombre de voyelles et le nombre de consonnes et attend pour qu'elles se terminent*)
+let clicked i deg deg2=
+  Lwt_main.run (Lwt.bind (Lwt.return(Js.to_string(i##.value))) (fun s -> (Lwt.join [(nbr_voys2 s deg) ;(nbr_cons2 s deg2)])))
 
+(*Dans le champs handler du bouton on met la fonction que l'on vient de créer qui nous permet donc de définir la fonction clicked comme une action qui se declenche*)
 let converterC_handler =
   Dom_html.handler begin
   fun evt -> (
-       clicked i d;
+       clicked i d d2;
     Js._true)
   end
 
